388efbd430d8222e340d8e8aefb305817f8781da
Shidul Islam <rahaddiu@gmail.com>
2023-05-01 21:52:21 +0600

    feat: test added for product

--- test/product.test.ts
+++ test/product.test.ts
@@ -3,20 +3,22 @@ import mocha from 'mocha';
 import { assert } from 'chai';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import { faker } from '@faker-js/faker';
 import server from '../src/server';
+import { expect } from 'chai';
 chai.use(chaiHttp);
 
 describe('Product suit', () => {
     it('should create a new product', async () => {
       const productInput = {
-        category_id: "6",
-        product_category_id: "3",
-        subcategory_id: "7",
-        name: 'watch',
-        slug: 'first-slug',
-        description: "Des",
-        price: 123,
-        status: "status"
+        category_id: faker.random.numeric(3),
+        product_category_id: faker.random.numeric(3),
+        subcategory_id: faker.random.numeric(3),
+        name: faker.lorem.word(),
+        slug: faker.lorem.word(),
+        description: faker.lorem.word(),
+        price: faker.random.numeric(3),
+        status: faker.lorem.word(),
       }
       const res = await(
       chai
@@ -24,6 +26,65 @@ describe('Product suit', () => {
         .post("/api/products")
         .send(productInput)
         );
-      console.log('res', res.body);
+      expect(res.body).to.be.an('object');
+    });
+
+    it('should return all product', async () => {
+      const productInput = {
+        category_id: faker.random.numeric(3),
+        product_category_id: faker.random.numeric(3),
+        subcategory_id: faker.random.numeric(3),
+        name: faker.lorem.word(),
+        slug: faker.lorem.word(),
+        description: faker.lorem.word(),
+        price: faker.random.numeric(3),
+        status: faker.lorem.word(),
+      }
+      const res = await(
+      chai
+        .request(server)
+        .post("/api/products")
+        .send(productInput)
+        );
+      const createProduct = res.body;
+
+      const resforProduct = await(
+        chai
+          .request(server)
+          .get("/api/products")
+          );
+      const resForGet = resforProduct.body;
+      expect(createProduct).to.be.an('object');
+      expect(resForGet).to.be.an('object');
+    });
+
+    it('should return single product', async () => {
+      const productInput = {
+        category_id: faker.random.numeric(3),
+        product_category_id: faker.random.numeric(3),
+        subcategory_id: faker.random.numeric(3),
+        name: faker.lorem.word(),
+        slug: faker.lorem.word(),
+        description: faker.lorem.word(),
+        price: faker.random.numeric(3),
+        status: faker.lorem.word(),
+      }
+      const res = await(
+      chai
+        .request(server)
+        .post("/api/products")
+        .send(productInput)
+        );
+      const createProduct = res.body;
+
+      const resforProduct = await(
+        chai
+          .request(server)
+          .get("/api/products")
+          .send(createProduct.product_id)
+          );
+      const resForGet = resforProduct.body;
+      expect(createProduct).to.be.an('object');
+      expect(resForGet).to.be.an('object');
     });
   });
